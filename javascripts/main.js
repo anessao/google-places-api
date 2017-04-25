6fc4f4ad3002c2a8160295142cfd0d379a340889
Anessa Ortner <anessa.ortner@gmail.com>
2017-04-24 21:32:28 -0500

    working api demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,33 @@
-console.log("Javascript loading");
+$(document).ready(() => {
+	const apiKey = "";
+
+	$("body").on("click", li, (e) => {
+		loadPlaces(e.target.innerHTML).then((data) => {
+			console.log(data);
+		}).catch((error) => {
+			console.log(error);
+		});
+	});
+
+	const loadAPIRequest = (dropdownType) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465,-86.767960&rankby=radius=50000&type=${dropdownType}&key=${apiKey}`).done((data) => resolve(data))
+				.fail((error) => reject(error));
+		});
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
