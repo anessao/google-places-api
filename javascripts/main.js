529ec49af1f2b2cda1e8d94d3d4b337d484aabb7
Anessa Ortner <anessa.ortner@gmail.com>
2017-04-25 19:27:06 -0500

    printing to the DOM - second api call

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,20 +1,44 @@
 $(document).ready(() => {
 	const apiKey = "";
+	const writeToDOM = (info) => {
+		$("#places").html("");
+		for (let i = 0; i < info.length; i++){
+			$("#places").append(`<a href="#"><div class="eachPlace" id="${info[i].place_id}">${info[i].name}</div></a>`);
+		}
+	}
+	const writeAddressToDom = (info) => {
+		let outputString = `<div>${info}</div>`;
+		$("#addresses").append(outputString);
+	}
 
 	$("body").on("click", "li", (e) => {
-		loadAPIRequest(e.target.innerHTML).then((data) => {
-			let info = data.results;
-			for (let i = 0; i < info.length; i++){
-				console.log(info[i].name);
-			}
+		loadAPIRequest(e.target.innerHTML).then((results) => {
+			writeToDOM(results);
 		}).catch((error) => {
 			console.log(error);
 		});
 	});
+	$("#places").on("click", ".eachPlace", (e) => {
+		let placeDetailId = e.target.id;
+		loadDetails(placeDetailId).then((result) => {
+			writeAddressToDom(result.formatted_address);
+		});
+	});
+
+	const loadDetails = (placeId) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${apiKey}`)
+			.done((data2) => {
+				resolve(data2.result);
+			}).fail((error2) => {
+				console.log(error2);
+			});
+		});
+	};
 
 	const loadAPIRequest = (dropdownType) => {
 		return new Promise ((resolve, reject) => {
-			$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465,-86.767960&radius=50000&type=${dropdownType}&key=${apiKey}`).done((data) => resolve(data))
+			$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465,-86.767960&radius=50000&type=${dropdownType}&key=${apiKey}`).done((data) => resolve(data.results))
 				.fail((error) => reject(error));
 		});
 	};
